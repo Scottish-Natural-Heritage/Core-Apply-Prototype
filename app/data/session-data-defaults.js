19478d9a5621d995a69fb63761a47bd18292fb79
Ricky Callaghan <98967492+RickyCallaghan@users.noreply.github.com>
2023-04-25 10:20:02 +0100

    fixing the text inputs suitable for ravens

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -10,6 +10,15 @@ module.exports = {
   'address' : 'NatureScot Headquarters Great Glen House Leachkin Road Inverness',
 
   'num-of-species' : '30',
-  'damage-details' : 'A thorough and precise explanation providing details of the damage experienced'
+
+  'damage-details' : 'A thorough and precise explanation providing details of the damage experienced',
+
+  'tried-details' : 'I have attempted different measures such as physical prevention, removing attractants, and creating a disturbance, with the aim of preventing further damage from occurring.',
+  'not-tried-details' : 'I do not believe any of these measures will work. There is nowhere for me to move the feed.',
+  
+  'previous-licence' : 'NS-RSD-12345',
+
+  'supporting-info' : 'There is no additional information I would like to add. However please contact me if anything else is required',
+
 
 }
